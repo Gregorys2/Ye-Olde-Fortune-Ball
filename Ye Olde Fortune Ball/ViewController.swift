9086ac465847622801b570b2be37f6536cc51699
Gregory Paul Sim <gregorys2@109d25lx1sbf8j7.local>
2019-02-05 15:11:58 -0700

    Initial Commit

---
+++ Ye Olde Fortune Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ye Olde Fortune Ball
+//
+//  Created by Gregory Paul Sim on 2019-02-05.
+//  Copyright © 2019 Gregory Paul Sim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
